504849e92f75e6ddfb9c8434b44c651043251666
Денис Рябцев <denys.riabtsev@nure.ua>
2021-04-04 11:34:28 +0300

    Add def. constructor LinkedList

--- SortedLinkedList/LinkedList.cs
+++ SortedLinkedList/LinkedList.cs
@@ -5,6 +5,12 @@
         public Node<T> Head { get; private set; }
         public Node<T> Tail { get; private set; }
         public int Count { get; private set; }
-
+        //constructor
+        public LinkedList()
+        {
+            Head = null;
+            Tail = null;
+            Count = 0;
+        }
     }
 }
