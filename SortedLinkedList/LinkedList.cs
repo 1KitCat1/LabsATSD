53b7444dec12ac4b124edaff08c988c6149c4df4
Денис Рябцев <denys.riabtsev@nure.ua>
2021-04-04 11:50:54 +0300

    Add method Add(one element) to LinkedList

--- SortedLinkedList/LinkedList.cs
+++ SortedLinkedList/LinkedList.cs
@@ -5,20 +5,38 @@
         public Node<T> Head { get; private set; }
         public Node<T> Tail { get; private set; }
         public int Count { get; private set; }
-        //constructor
+        //constructors
         public LinkedList()
         {
             Head = null;
             Tail = null;
             Count = 0;
         }
-
+        
         public LinkedList(T data)
         {
             var node = new Node<T>(data);
             Head = node;
             Tail = node;
             Count = 1;
+        }
+        //
+        public void Add(T data)
+        {
+            var node = new Node<T>(data);
+            if (Tail != null)
+            {
+                Tail.Next = node;
+                Tail = node;
+                Count++;
+            }
+            else
+            {
+                Head = node;
+                Tail = node;
+                Count = 1;
+            }
+            
         }
     }
 }
