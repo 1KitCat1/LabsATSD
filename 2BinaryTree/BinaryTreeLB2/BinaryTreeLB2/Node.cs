6959e91b38319d4d0ed6f5561dec8a163aec9bfa
Денис Рябцев <denys.riabtsev@nure.ua>
2021-04-21 13:35:16 +0300

    Finished Node

--- 2BinaryTree/BinaryTreeLB2/BinaryTreeLB2/Node.cs
+++ 2BinaryTree/BinaryTreeLB2/BinaryTreeLB2/Node.cs
@@ -1,7 +1,37 @@
-﻿namespace BinaryTreeLB2
+﻿using System;
+
+namespace BinaryTreeLB2
 {
-    public class Node
+    public class Node<T> : IComparable
+        where T: IComparable
     {
-        
+        public T Data { get; set; }
+        public Node<T> Left { get; set; }
+        public Node<T> Right { get; set; }
+
+        public Node(T data)
+        {
+            Data = data;
+        }
+
+        public Node(T data, Node<T> left, Node<T> right)
+        {
+            Data = data;
+            Left = left;
+            Right = right;
+        }
+
+        public int CompareTo(object? obj)
+        {
+            if (obj is Node<T> other)
+            {
+                return Data.CompareTo(other.Data);
+            }
+            else
+            {
+                Console.WriteLine("Cannot compare properly");
+                return -1;
+            }
+        }
     }
 }
